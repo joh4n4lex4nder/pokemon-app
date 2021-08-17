4e8f4fc6d7a3148c9802954f64647fb978065fef
joh4n4lex4nder <joh4n4lex4nder@gmail.com>
2021-08-17 00:07:42 -0500

    App completa y funcional

--- scripts/app.js
+++ scripts/app.js
@@ -1,81 +1,48 @@
-//const API_URL = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
-//const API_URL = 'https://pokeapi.co/api/v2/pokemon/151';
-const API_URL = 'https://pokeapi.co/api/v2/pokemon/10';
-//const API_URL = 'https://pokeapi.co/api/v2/pokemon/?limit=10';
-const main = document.getElementById('main');
-
-//Evento disparado cuando el documento HTML ha sido completamente cargado y luego continúa con el JavaScript
 document.addEventListener('DOMContentLoaded', () => {
-    //const random = getRandomInt(1, 151);
-    getData(API_URL);
+    const random = getRandomInt(1, 151);
+    getData(random);
 })
 
-/*
-//Función flecha para generar números aleatorios entre 1 y 151
+//Función para retornar un número aleatorio
 const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-*/
 
-//Función asíncrona que envía la petición para consumir la información de la API y que espera hasta obtener toda la data para luego mostrarla
-const getData = async (url) => {
+const getData = async (id) => {
     try {
-        const respuesta = await fetch(url);
-        const data = await respuesta.json();
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`); //Fetch por defecto hace el consumo en GET(solo lectura)
+        const data = await res.json();
         console.log(data);
+
         const pokemon = {
             img: data.sprites.other.dream_world.front_default,
-            nombre: data.name
+            nombre: data.name,
+            hp: data.stats[0].base_stat,
+            experiencia: data.base_experience,
+            ataque: data.stats[1].base_stat,
+            especial: data.stats[3].base_stat,
+            defensa: data.stats[2].base_stat
         }
-        showData(pokemon);
+        paintCard(pokemon);
     } catch (error) {
-          console.log(error);
+        console.log(error);
     }
 }
 
-/*
-const getData = (url) => {
-    const peticion = fetch(url);
-    peticion.then(res => res.json()) 
-            .then(data => showData(data.sprites.other.dream_world.front_default))
-}
-*/
+const paintCard = (pokemon) => {
+    //console.log(pokemon); Se imprime el objeto pokemon con toda la información
+    const main = document.querySelector('.container');
+    const template = document.getElementById('template-card').content;
+    const cloneTemplate = template.cloneNode(true);
+    const fragment = document.createDocumentFragment();
 
-function showData(pokemon) {
-    console.log(pokemon)
-    main.innerHTML = ''
+    cloneTemplate.querySelector('.card-body-img').setAttribute('src', pokemon.img);
+    cloneTemplate.querySelector('.card-body-title').innerHTML = `${pokemon.nombre} <span>${pokemon.hp} hp</span>`;
+    cloneTemplate.querySelector('.card-body-text').textContent = pokemon.experiencia + ' Exp';
+    cloneTemplate.querySelectorAll('.card-footer-social h3')[0].textContent = pokemon.ataque;
+    cloneTemplate.querySelectorAll('.card-footer-social h3')[1].textContent = pokemon.especial;
+    cloneTemplate.querySelectorAll('.card-footer-social h3')[2].textContent = pokemon.defensa;
 
-    pokemon.forEach((element) => {
-        const{name, front_default} = element
-        const pokemonElement = document.createElement('div')
-        pokemonElement.classList.add('card')
-        pokemonElement.innerHTML = `
-            <div class="card">
-                <img src="./images/bg-pattern-card.svg" alt="" class="card-header">
-                <div class="card-body">
-                    <img src="${front_default}" alt="" class="card-body-img">
-                    <h2 class="card-body-title">
-                        ${name}
-                        <span>26</span>
-                    </h2>
-                    <p>Descripción</p>
-                </div>
-                <div class="card-footer">
-                    <div class="card-footer-social">
-                        <h3>80K</h3>
-                        <p>Followers</p>
-                    </div>
-                    <div class="card-footer-social">
-                        <h3>803K</h3>
-                        <p>Likes</p>
-                    </div>
-                    <div class="card-footer-social">
-                        <h3>1.4K</h3>
-                        <p>Photos</p>
-                    </div>
-                </div>
-            </div>
-        `
-        main.appendChild(pokemonElement)
-    })
+    fragment.appendChild(cloneTemplate);
+    main.appendChild(fragment);
 }
